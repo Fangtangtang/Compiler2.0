041f7569c46f423f530d319c1082ca4e7fd3cab3
Fangtangtang <2167295472@qq.com>
2024-05-23 00:47:03 +0800

    [fix] remove dead block when building CFG

--- src/midend/optimizer/CFGBuilder.java
+++ src/midend/optimizer/CFGBuilder.java
@@ -5,8 +5,11 @@ import ir.IRRoot;
 import ir.function.Function;
 import ir.stmt.terminal.Branch;
 import ir.stmt.terminal.Jump;
+import utility.Pair;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 /**
@@ -45,13 +48,66 @@ public class CFGBuilder {
     }
 
     private void buildOnFunction(Function function) {
-        //对每一个basic block添加前驱后继
+        // eliminate some dead block here
+        // bb -> <predecessorList , successorList>
+        HashMap<String, Pair<ArrayList<String>, ArrayList<String>>> familyTree = new HashMap<>();
         for (Map.Entry<String, BasicBlock> entry : function.blockMap.entrySet()) {
             BasicBlock block = entry.getValue();
+            if (!familyTree.containsKey(block.label)) {
+                familyTree.put(block.label, new Pair<>(new ArrayList<>(), new ArrayList<>()));
+            }
             if (block.tailStmt instanceof Jump jump) {
                 if (jump.target == null) {
                     jump.target = function.blockMap.get(jump.targetName);
                 }
+                if (!familyTree.containsKey(jump.target.label)) {
+                    familyTree.put(jump.target.label, new Pair<>(new ArrayList<>(), new ArrayList<>()));
+                }
+                familyTree.get(block.label).getSecond().add(jump.target.label);
+                familyTree.get(jump.target.label).getFirst().add(block.label);
+            } else if (block.tailStmt instanceof Branch branch) {
+                familyTree.get(block.label).getSecond().add(branch.trueBranch.label);
+                familyTree.get(block.label).getSecond().add(branch.falseBranch.label);
+                if (!familyTree.containsKey(branch.trueBranch.label)) {
+                    familyTree.put(branch.trueBranch.label, new Pair<>(new ArrayList<>(), new ArrayList<>()));
+                }
+                if (!familyTree.containsKey(branch.falseBranch.label)) {
+                    familyTree.put(branch.falseBranch.label, new Pair<>(new ArrayList<>(), new ArrayList<>()));
+                }
+                familyTree.get(branch.trueBranch.label).getFirst().add(block.label);
+                familyTree.get(branch.falseBranch.label).getFirst().add(block.label);
+            }
+        }
+        HashSet<String> eliminateWorkList = new HashSet<>();
+        for (Map.Entry<String, Pair<ArrayList<String>, ArrayList<String>>> entry : familyTree.entrySet()) {
+            if (entry.getValue().getFirst().size() == 0) {
+                eliminateWorkList.add(entry.getKey());
+            }
+        }
+        eliminateWorkList.remove(function.entry.label);
+        while (!eliminateWorkList.isEmpty()) {
+            String blockName = eliminateWorkList.iterator().next();
+            eliminateWorkList.remove(blockName);
+            function.blockMap.remove(blockName);
+            for (String successor : familyTree.get(blockName).getSecond()) {
+                ArrayList<String> list = familyTree.get(successor).getFirst();
+                ArrayList<String> newList = new ArrayList<>();
+                for (int i = 0; i < list.size(); i++) {
+                    if (!blockName.equals(list.get(i))) {
+                        newList.add(list.get(i));
+                    }
+                }
+                if (newList.size()==0){
+                    eliminateWorkList.add(successor);
+                }else {
+                    familyTree.get(successor).setFirst(newList);
+                }
+            }
+        }
+        //对每一个basic block添加前驱后继
+        for (Map.Entry<String, BasicBlock> entry : function.blockMap.entrySet()) {
+            BasicBlock block = entry.getValue();
+            if (block.tailStmt instanceof Jump jump) {
                 block.successorList.add(jump.target);
                 jump.target.predecessorList.add(block);
             } else if (block.tailStmt instanceof Branch branch) {
